5d19021364e609075801522e26f6b59f42b64c4f
szeroxxx <szeroxxx0@gmail.com>
2025-05-25 02:02:32 +0530

    visitor side

---
+++ src/pages/debug.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DebugPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Debug Page</h1>
+      <p>This is a debug page for development purposes.</p>
+    </div>
+  );
+};
+
+export default DebugPage;
