0da61493861569d7a69e57de3e2b1d8c34a2859d
szeroxxx <szeroxxx0@gmail.com>
2025-05-28 12:44:40 +0530

    domain add for image

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,11 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
-    domains: ['localhost','images.unsplash.com'],
+    domains: ["localhost", "images.unsplash.com", "avilamycotour.es"],
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'mycotur-secret-key',
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "mycotur-secret-key",
     NEXTAUTH_BACKEND_URL: process.env.NEXTAUTH_BACKEND_URL,
   },
 };
